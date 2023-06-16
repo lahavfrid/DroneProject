d20b4e857bde8323aad337b7ddd3e08b2d0897f3
roeikrinitz <95423288+roeikrinitz@users.noreply.github.com>
2023-06-16 18:16:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     version='0.1',
     packages=find_packages(),
     author='Roei krinitz, Lahav fridlend, Alon Spector',
+     install_requires=[
+        'gymnasium',
+    ],
     author_email='your@email.com',
     description='Description of your project',
     url='https://github.com/lahavfrid/DroneProject',

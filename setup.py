8fd0c9fbd8946f60eebbee809684e5c14e9de298
roeikrinitz <95423288+roeikrinitz@users.noreply.github.com>
2023-06-16 17:04:07 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='YourPackageName',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'dependency1',
+        'dependency2',
+    ],
+    author='Your Name',
+    author_email='your@email.com',
+    description='Description of your project',
+    url='https://github.com/your_username/your_project',
+)

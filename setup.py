e84e50cc94f3ea11cb3cfe05e44f7b572da9098f
roeikrinitz <95423288+roeikrinitz@users.noreply.github.com>
2023-06-16 18:08:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@ setup(
     name='YourPackageName',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'dependency1',
-        'dependency2',
-    ],
     author='Roei krinitz, Lahav fridlend, Alon Spector',
     author_email='your@email.com',
     description='Description of your project',
